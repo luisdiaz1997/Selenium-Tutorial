561a645d8115fa8f074d7456a3642bf834b94290
luisdiaz1997 <luisfcd2013@hotmail.com>
2020-07-12 13:34:30 -0700

    Added storyboard

--- functional_test.py
+++ functional_test.py
@@ -2,8 +2,15 @@ from selenium import webdriver
 
 browser = webdriver.Firefox()
 
+# Luis knows of this website and opens it
 browser.get('http://localhost:5000')
 
-assert "Hello" in browser.title, "Title is not Hello"
+# He checks the title says “Inventory”
+assert "Hello" in browser.title, "Title was " + browser.title
+
+# He adds a shirt and the number of shirts increases + 1
+# He changes the number of pants to 5
+# He changes the number of shirts back to zero and this says OUT-OF-STOCK
+# He checks the total inventory is the sum of all numbers (example 20)
 
 browser.quit()
